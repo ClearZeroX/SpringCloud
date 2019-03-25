f97cf908d565e1c002215ec89aabf58dc476b4a4
ZhaLiCun <t_zhalc@channelsoft.com>
2019-03-25 16:53:27 +0800

    eureka服务的注册与发现

---
+++ src/main/java/com/zlc/SpringCloud/config/WebExceptionAspect.java
@@ -0,0 +1,18 @@
+package com.zlc.SpringCloud.config;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : ZLC
+ * @create : 2019-03-22 16:11
+ * @desc : 通过切面实现异常捕捉
+ **/
+/*@Aspect*/
+@Component
+public class WebExceptionAspect {
+
+
+
+
+
+}
